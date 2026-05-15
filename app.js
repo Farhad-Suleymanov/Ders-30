880e3cfe9b696a72a09bd54e4d39aa424e2d300b
Farhad-Suleymanov <fsuleymanov007@gmail.com>
2026-05-15 21:56:36 +0400

    firstcommit

---
+++ app.js
@@ -0,0 +1,17 @@
+let arr = [1,2,6,3,2,7,8,3,1,8,6,4,3,1,8,9,9];
+arr.sort((a,b) => a - b);
+console.log("Original massiv: "+ arr);
+let arr2 = []
+let i = 1;
+while (i<arr.length){
+    if (arr[i] == arr[i-1]){
+        arr2.push(arr[i]);
+        while (arr[i] == arr[i-1]) {
+            i++
+            continue
+        }
+
+    }
+    i++
+}
+console.log("Tekrarlanan ededlerden olan massiv: " + arr2);
